b1c2e7634be460cfb177e26d035bc92a73cd2142
Raycode22 <117015158+raycode22@users.noreply.github.com>
2024-04-06 23:50:58 +0800

    Add Homepage

--- index.php
+++ index.php
@@ -4,38 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign in - Google Accounts</title>
-    <link rel="stylesheet" href="styles.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <title>Home</title>
+
 </head>
 
 <body>
-    <div class="container">
-        <div class="logo">
-            <span class="material-symbols-outlined">
-                home_app_logo
-            </span>
-        </div>
-        <h1>Sign in</h1>
-        <form action="#">
-            <div class="input-field">
-                <label for="email">Email or phone number</label>
-                <input type="email" name="email" id="email" required>
-            </div>
-            <div class="input-field">
-                <label for="password">Password</label>
-                <input type="password" name="password" id="password" required>
-            </div>
-            <div class="checkbox-field">
-                <input type="checkbox" id="remember-me">
-                <label for="remember-me">Remember me</label>
-            </div>
-            <button type="submit">Sign in</button>
-            <a href="#">Forgot password?</a>
-        </form>
 
-        <p>Create an account. <a href="#">Sign up for Google</a></p>
-    </div>
     <footer>
         <p>©2023 QuickRef.ME</p>
     </footer>
